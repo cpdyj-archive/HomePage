759dc4b2cf639151d57ef147840d379f65b295bc
yiji <canpodeyiji@gmail.com>
2017-08-08 11:50:27 +0800

    add.

---
+++ config.js
@@ -0,0 +1,10 @@
+var debug=false;
+var websites=[
+	{title:"Baidu",url:"https://m.baidu.com",img:"img/baidu.ico"},
+	{title:"Google",url:"https://google.com",img:"img/google.png"},
+	{title:"Github",url:"https://github.com",img:"img/github.svg"},
+	{title:"Vultr",url:"http://vultr.com",img:"img/vultr.ico"},
+	{title:"DigitalOcean",url:"http://digitalocean.com",img:"img/digitalocean.png"},
+	{title:"Dnspod",url:"http://dnspod.cn",img:"img/dnspod.ico"},
+	{title:"Gitter",url:"https://gitter.im",img:"img/gitter.png"},
+	];
